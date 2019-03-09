1a3f5ed122d3773448345fc61ea4196d505f310d
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-03-09 13:53:48 -0700

    setting logic with pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+            // FIRST: we need the data to work with (Q's and A's
+
+            // array of objects = [q1, q2, q3, q4, q5]
+
+            // q1 {question= (question), answer array [ans 1, ans 2, ...], id which index is correct}
+            // continue w/ all q's. 
+
+            // SECOND: we need to create the display via functions
+            // function to print cards (loop through data to create & append to DOM)
+            // each card: grab q, loop through and print answers
+            // answers need to have clickable bullet/box/etc
+            // LOOKUP: buttons/bullets (something to select)
+            // 
+        };
